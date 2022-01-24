183351393e82ab7e7e08be2559e98765b1d450c8
Miracleyuuki <98302484+Miracleyuuki@users.noreply.github.com>
2022-01-24 13:52:49 +0800

    Add files via upload

---
+++ text.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+window.onload=function(){
+
+  var r =confirm("Awak!!...Harap awak suka ^_^ V");
+  if (r == true) {
+  document.getElementById("HB").play();
+
+}
+}
+
+</script>
